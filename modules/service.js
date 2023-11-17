28c120aa68c0a0e1ddb6ffae6cf5815d86bcab7e
Zhanpeng Tong <tong.zh@northeastern.edu>
2023-11-16 20:59:48 -0500

    try to edit service.js

--- modules/service.js
+++ modules/service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const MODULES_URL = `${API_BASE}/modules`;
 export const deleteModule = async (moduleId) => {
   const response = await axios
     .delete(`${MODULES_URL}/${moduleId}`);
